77fde733d4af78b6bf2d88cc38ac225982b8e519
liban <botanlee515@gmail.com>
2021-03-09 12:04:06 +0100

    Done

---
+++ ex1/exercise.js
@@ -0,0 +1,9 @@
+
+
+  var js =  JSON.parse('{ "id": 1, "title":"this is a note", "content":"This is the amazing content of the note!", "name" : "Liban" }');
+  console.log(js);
+
+  var obj =JSON.stringify(js);
+  console.log(obj)
+
+  
